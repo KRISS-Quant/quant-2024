fce3eeae4291139ea79c51b65126de917eab16b4
Sangha Lee <sanghaleem@gmail.com>
2024-11-24 17:56:07 +0800

    CandlestickChart page connection

---
+++ app/candlestick/page.tsx
@@ -0,0 +1,9 @@
+import CandlestickChart from "@/components/CandlestickChart";
+
+export default function CandlestickPage() {
+  return (
+    <div className="flex justify-center items-center min-h-screen p-4">
+      <CandlestickChart />
+    </div>
+  );
+}
